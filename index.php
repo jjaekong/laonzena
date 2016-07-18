192df7a139e468dd403760a0af671e606131977d
Kim Jaehong <jjaekong@gmail.com>
2016-07-18 20:34:54 +0900

    레이아웃 작성 중...

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LAON ZENA - PRESTIGE WEDDING</title>
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="/assets/css/default.css" rel="stylesheet">
+</head>
+<body>
+    <header id="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-2">
+                    <ul class="sns visible-lg-block">
+                       <li><a href="#"><img src="/assets/images/ico_twitter.png" alt="트위터"></a></li>
+                       <li><a href="#"><img src="/assets/images/ico_weibo.png" alt="웨이보"></a></li>
+                       <li><a href="#"><img src="/assets/images/ico_wechat.png" alt="위쳇"></a></li>
+                    </ul>
+                    <button class="btn btn-menu hidden-lg" type="button">
+                        <span class="bar"></span>
+                        <span class="bar"></span>
+                        <span class="bar"></span>
+                    </button>
+                </div>
+                <div class="col-xs-8">
+                    <h1 class="logo"><a href="/"><img src="/assets/images/logo.png" alt="라온제나"></a></h1>
+                </div>
+                <div class="col-xs-2">
+                    <a class="btn btn-user hidden-lg" href="#"><img src="/assets/images/ico_user.png" alt=""></a>
+                    <a class="reserve visible-lg-block" href="#">Reservation</a>
+                    <ul class="member visible-lg-block">
+                        <li class="login"><a href="#">로그인</a></li>
+                        <li class="join"><a href="#">회원가입</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <nav id="gnb" tabindex="-1">
+        <div class="container">
+
+        </div>
+    </nav>
+    <main id="content" tabindex="-1">
+
+    </main>
+    <footer id="footer">
+        <div class="container">
+            <p>(C) 2016 LAON ZENA. ALL RIGHTS RESERVED.</p>
+        </div>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+</body>
+</html>
