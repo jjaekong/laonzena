f1c05a1314ce0e175c4606149504af40cc65df99
Kim Jaehong <jjaekong@gmail.com>
2016-07-25 17:05:23 +0900

    git ftp test

--- hair/artist.php
+++ hair/artist.php
@@ -21,6 +21,7 @@
                     </div>
                 </div>
                 <div class="col-xs-12 col-lg-7">
+                    아티스트 설명영역
                 </div>
             </div>
         </div>
