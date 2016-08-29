86bdf540bd6188285219952efb1dd5e6b3c2340b
mk-kimjh <mk.kimjh@gmail.com>
2016-08-29 15:42:15 +0900

    푸터 언어선택 박스 추가

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,21 @@
 <footer id="footer">
     <div class="container">
-        <p>(C) 2016 LAON ZENA. ALL RIGHTS RESERVED.</p>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <p class="copy">(C) 2016 LAON ZENA. ALL RIGHTS RESERVED.</p>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <div class="lang dropup">
+                    <button class="btn btn-default dropdown-toggle" type="button" id="btn-lang" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        語言
+                        <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="btn-lang">
+                        <li><a href="/">中國語</a></li>
+                        <li><a href="/kr">한국어</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </footer>
